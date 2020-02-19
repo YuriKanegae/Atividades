3e190937b720f18b4585ec7dc32f8e09ae9bce8c
YuriKanegae <yurikanegae@gmail.com>
2020-02-19 17:18:29 -0300

    V1

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8"/>
+        <title>Minhas atividades</title>
+    </head>
+    <body>
+        <h2>Minhas atividades:</h2>
+    </body>
+</html>
