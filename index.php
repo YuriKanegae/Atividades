20fc7aafb4c744cdc42cc4eb28241fd56239d30c
YuriKanegae <yurikanegae@gmail.com>
2020-03-03 16:53:05 -0300

    20200303

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset = "UTF-8"/>
-        <title>Minhas atividades</title>
+        <meta charset = "UTF-8" />
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+        <title>Portfolio de atividades</title>
     </head>
     <body>
-        <h2>Minhas atividades:</h2>
+        <div class = "container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" style = "margin-bottom: 30px;">
+                <span class = "navbar-brand mb-0 h1">Yuri Kanegae</span>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/YuriKanegae">GIT</a>
+                        </li>
+                        <li class="nav-item">
+                            <p class="nav-link">Prontuário: 1890794</p>
+                        </li>
+						<li class = "nav-item">
+							<a href = "https://arq.ifsp.edu.br/portal/" class = "nav-link">Campos Araraquara</a>
+						</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <script src = "js/jquery-3.2.1.min.js"></script>
+        <script src = "js/popper.min.js"></script>
+        <script src = "js/bootstrap.min.js"></script>
+        <script src = "js/validaForm.js"></script>
     </body>
 </html>
