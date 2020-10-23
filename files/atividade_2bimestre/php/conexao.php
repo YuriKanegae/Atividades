4c8bb445dfb9bf188a7580842a62190f660f6e63
YuriKanegae <yurikanegae>
2020-10-22 23:28:41 -0300

    dbfree

--- files/atividade_2bimestre/php/conexao.php
+++ files/atividade_2bimestre/php/conexao.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
-    $usuario = "root";
-    $senha = "usbw";
-    $bd = "musicplayer";
+    $host = "db4free.net:3306";
+    $usuario = "amandayuri";
+    $senha = "musicplayer42";
+    $bd = "musicplayer90119";
 
     if(!$conexao = mysqli_connect($host, $usuario, $senha, $bd)){
         echo "Conexão com Banco de dados falhou";
